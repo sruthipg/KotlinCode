d696f687fe4902642de17320a26ca6f51f0a281d
sruthipg <sruthisruthikrishnan@gmail.com>
2020-06-02 23:47:52 +0530

    Added Updated ModelClass

--- app/src/main/java/com/example/whiterabittest/db/model/EmployeeData.kt
+++ app/src/main/java/com/example/whiterabittest/db/model/EmployeeData.kt
@@ -1,19 +1,21 @@
 package com.example.whiterabittest.db.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import AddressData
+import CompanyData
+import androidx.room.*
 
 @Entity
 data class EmployeeData(
-    @PrimaryKey(autoGenerate = false)
-    val id : Int,
-    val name : String,
-    val username : String,
-    val email : String,
-    val profile_image : String,
-    val phone : String,
-    val website : String
+
+    @PrimaryKey val id : Int,
+    val name : String?,
+    val username : String?,
+    val email : String?,
+    val profile_image : String?,
+    val phone : String?,
+    val website : String?
 
 
-)//    val company : CompanyData
-//    val address : AddressData,
+//    val company : CompanyData,
+//    val address : AddressData
+)
